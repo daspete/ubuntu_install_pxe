2f0e75877db777f7cb9f9f48555080c31f84f441
daspete <daspetemail@gmail.com>
2014-06-29 13:12:50 +0200

    Update createPXE.sh

--- createPXE.sh
+++ createPXE.sh
@@ -14,8 +14,8 @@ echo "------------------------------------------------------------------------"
 
 echo -e "$redBlack"; source ./scripts/installDep.sh
 echo -e "$greenBlack"; source ./scripts/configDHCP.sh
-echo -e "$redBlack"; source ./scripts/createSyslinux.sh
-echo -e "$greenBlack"; source ./scripts/configTFTP.sh
+echo -e "$redBlack"; source ./scripts/configTFTP.sh
+echo -e "$greenBlack"; source ./scripts/createSyslinux.sh
 echo -e "$redBlack"; source ./scripts/copyImage.sh
 echo -e "$greenBlack"; source ./scripts/configNFS.sh
 echo -e "$redBlack"; source ./scripts/makeBootMenu.sh
