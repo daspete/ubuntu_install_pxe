9781a1f4274165200bdc81539b02446fcb18ccfc
daspete <daspetemail@gmail.com>
2014-06-29 13:09:24 +0200

    Update createSyslinux.sh

--- scripts/createSyslinux.sh
+++ scripts/createSyslinux.sh
@@ -1,7 +1,7 @@
 echo -n "Copying SYSLinux PXE-Boot-Image and configuration... "
 
 if [ ! -f "${tftpRoot}/menu.c32" ]; then
-        cp /usr/lib/syslinux/menu.c32 /${tftpRoot}/menu.c32
+        cp /usr/lib/syslinux/menu.c32 ${tftpRoot}/menu.c32
 fi
 
 if [ ! -f "${tftpRoot}/pxelinux.0" ]; then
